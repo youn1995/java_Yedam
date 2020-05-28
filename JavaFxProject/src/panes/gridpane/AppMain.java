710cc8cdb2c1fd7d79866517753883d2e17a0f05
SungyounKim <sg17990@gmail.com>
2020-05-28 22:48:57 +0900

    learn stack,girdpane

---
+++ JavaFxProject/src/panes/gridpane/AppMain.java
@@ -0,0 +1,23 @@
+package panes.gridpane;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Root.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("GridPane Sample");
+		primaryStage.show();
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ JavaFxProject/src/panes/stackpane/AppMain.java
@@ -0,0 +1,24 @@
+package panes.stackpane;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Root.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("StackPane Sample");
+		primaryStage.show();
+		
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
