4550f74d0c7a96a67db82ee3abd7661f1c7bf69c
SungyounKim <sg17990@gmail.com>
2020-06-17 10:28:04 +0900

    aa

---
+++ ProjectFirst/src/com/yedam/morning/Code20200617.java
@@ -0,0 +1,30 @@
+package com.yedam.morning;
+
+public class Code20200617 {
+	public static void main(String[] args) {
+
+		int[] arr = new int[5];
+		while (true) {
+			for (int i = 0; i < 5; i++) {
+				arr[i] = (int) (Math.random() * 5) + 1;
+				for (int j = 0; j < i; j++) {
+					if (arr[i] == arr[j]) {
+						arr[i] = (int) (Math.random() * 5) + 1;
+						i--;
+					}
+
+				}
+			}
+			if(arr[4] == arr[3] || arr[3] == arr[2] || arr[0] == arr[1] || arr[1] == arr[4]) {
+				break;
+			}
+
+			for (int i = 0; i < arr.length; i++) {
+				System.out.print(arr[i] + ", ");
+				arr[i] = 0;
+			}
+			System.out.println();
+	
+		}
+	}
+}
